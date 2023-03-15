20f26311cd4be8482194e1fc11cb31bbdb3eac3b
miron9 <mironq@o2.pl>
2023-03-15 22:29:38 +0000

    Inject WiFi name and password; prompt for Wireguard Network range to be used

--- run_me.sh
+++ run_me.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
 # TODO
-# inject WIFI password and config
 # get CIDR for WG from user
 
-WIREGUARD_PORT=51820
-WIREGUARD_START_IP=192.168.5.0/24
+#WIREGUARD_PORT=51820
+#WIREGUARD_CIDR=192.168.5.0/24
 
-CIDR=$(echo ${WIREGUARD_START_IP}|cut -d/ -f2)
-
-SERVICE_FILE=./wfh-anywhere-vpn-router.service
-OUTPUT_DIR=./output
-USER_ANSWERS_FILE=${OUTPUT_DIR}/configuration.ini
-
+# Functions START
 prerequisites(){
     mkdir -p ${OUTPUT_DIR}
     if [[ ! -f ${USER_ANSWERS_FILE} ]]; then
@@ -26,15 +20,16 @@ install_requirements() {
     apt install isc-dhcp-common dnsmasq hostapd iptables iproute2 wireguard sed gettext-base -y
 }
 
-hostapd() {
-    # TODO inject pass and other config
-    cp ./hostapd.conf /etc/hostapd/
+hostapd_config() {
+    node_id=${1}
+    export HOTSPOT_NAME HOTSPOT_PASSWORD
+    cat ./hostapd.conf | envsubst > ${OUTPUT_DIR}/${node_id}/hostapd.conf
 }
 
-systemd() {
-    cp ./vpn.service /usr/lib/systemd/system/${WIREGUARD_INTERFACE_NAME}.service
-    systemctl enable ${WIREGUARD_INTERFACE_NAME}.service
-}
+#systemd() {
+#    cp ./vpn.service /usr/lib/systemd/system/${WIREGUARD_INTERFACE_NAME}.service
+#    systemctl enable ${WIREGUARD_INTERFACE_NAME}.service
+#}
 
 wireguard_router_config() {
     node_id=${1}
@@ -70,7 +65,7 @@ PostDown = ip rule del iif ${WIREGUARD_INTERFACE_NAME} table 123 priority 456
 # previous node
 [Peer]
 PublicKey = $(cat $OUTPUT_DIR/${previous_node_id}/public)
-AllowedIPs = $(cat $OUTPUT_DIR/${previous_node_id}/wireguard_ip)/${CIDR}
+AllowedIPs = $(cat $OUTPUT_DIR/${previous_node_id}/wireguard_ip)/${WIREGUARD_NETMASK}
 $( [[ -f $OUTPUT_DIR/${previous_node_id}/public_ip ]] && echo "Endpoint = $(cat $OUTPUT_DIR/${previous_node_id}/public_ip):${WIREGUARD_PORT}" )
 $( [[ ! -f $OUTPUT_DIR/${node_id}/public_ip ]] && echo "PersistentKeepalive = 25" )
 # next node
@@ -104,7 +99,7 @@ PostDown = iptables -t nat -D POSTROUTING ! -o ${WIREGUARD_INTERFACE_NAME} -m ma
 # previous node
 [Peer]
 PublicKey = $(cat $OUTPUT_DIR/${previous_node_id}/public)
-AllowedIPs = $(cat $OUTPUT_DIR/${previous_node_id}/wireguard_ip)/${CIDR}
+AllowedIPs = $(cat $OUTPUT_DIR/${previous_node_id}/wireguard_ip)/${WIREGUARD_NETMASK}
 $( [[ -f $OUTPUT_DIR/${previous_node_id}/public_ip ]] && echo "Endpoint = $(cat $OUTPUT_DIR/${previous_node_id}/public_ip):${WIREGUARD_PORT}" )
 $( [[ ! -f $OUTPUT_DIR/${node_id}/public_ip ]] && echo "PersistentKeepalive = 25" )
 EOF
@@ -132,13 +127,13 @@ $(cat ./wfh-anywhere-vpn.sh | envsubst '${WG_IP},${WG_IF_NAME}' | sed 's/\$/\\$/
 EOF
 chmod u+x /usr/local/bin/wfh-anywhere-vpn.sh
 
+cat << EOF > /etc/hostapd/hostapd.conf
+$(cat ${OUTPUT_DIR}/${NODE_ID}/hostapd.conf)
+EOF
+
 systemctl enable wfh-anywhere-vpn-${WIREGUARD_INTERFACE_NAME}
 systemctl start wfh-anywhere-vpn-${WIREGUARD_INTERFACE_NAME}
 systemctl status wfh-anywhere-vpn-${WIREGUARD_INTERFACE_NAME}
-
-#systemctl enable wg-quick@${WIREGUARD_INTERFACE_NAME}
-#systemctl start wg-quick@${WIREGUARD_INTERFACE_NAME}
-#systemctl status wg-quick@${WIREGUARD_INTERFACE_NAME}
 EOFX
 chmod u+x ./${OUTPUT_DIR}/${NODE_ID}/generated_wireguard_vpn_install_script.sh
 }
@@ -231,6 +226,8 @@ get_and_save_user_input_if_missing() {
 collect_data_from_user() {
     get_and_save_user_input_if_missing "Enter node count" NODE_COUNT shared 3
     get_and_save_user_input_if_missing "Wireguard interface name" WIREGUARD_INTERFACE_NAME shared "wgvpn0"
+    get_and_save_user_input_if_missing "Wireguard network CIDR" WIREGUARD_CIDR shared "192.168.200.0/24"
+    get_and_save_user_input_if_missing "Wireguard port" WIREGUARD_PORT shared "51820"
     get_and_save_user_input_if_missing "WiFi hotspot name/SSID" HOTSPOT_NAME shared "RoadWarrior"
     get_and_save_user_input_if_missing "WiFi hotspot password" HOTSPOT_PASSWORD shared "-"
 }
@@ -252,20 +249,36 @@ get_next_ip() {
 
     echo "${o1}.${o2}.${o3}.$((o4+${2}*10))" > ${OUTPUT_DIR}/${2}/wireguard_ip
 }
+# Functions END
 
+
+
+#=============================================================================
+
+
+
+# Prepare and collect data START
+SERVICE_FILE=./wfh-anywhere-vpn-router.service
+OUTPUT_DIR=./output
+USER_ANSWERS_FILE=${OUTPUT_DIR}/configuration.ini
 
 install_requirements
 prerequisites
 collect_data_from_user
+# Prepare and collect data END
 
+
+# Generate configs and scripts START
 source ${USER_ANSWERS_FILE}
+
+WIREGUARD_NETMASK=$(echo ${WIREGUARD_CIDR}|cut -d/ -f2)
 
 # Generate keys and IPs that will be required for template generation
 NEXT_NODE_MUST_HAVE_PUBLIC_IP=false
 for ((NODE_ID=1; NODE_ID<=${NODE_COUNT}; NODE_ID++)); do
     mkdir -p ${OUTPUT_DIR}/${NODE_ID}
     wireguard_generate_keys ${NODE_ID}
-    get_next_ip "${WIREGUARD_START_IP}" ${NODE_ID}
+    get_next_ip "${WIREGUARD_CIDR}" ${NODE_ID}
 
     echo "Node ${NODE_ID}"
 
@@ -301,6 +314,7 @@ done
 # Render templates
 for ((NODE_ID=1; NODE_ID<=${NODE_COUNT}; NODE_ID++)); do
     if [[ ${NODE_ID} == 1 ]]; then
+        hostapd_config 1
         wireguard_router_config ${NODE_ID}
         generate_router_node_install_script
     elif [[ ${NODE_ID} < ${NODE_COUNT} ]]; then
@@ -311,4 +325,4 @@ for ((NODE_ID=1; NODE_ID<=${NODE_COUNT}; NODE_ID++)); do
         generate_every_next_node_install_script
     fi
 done
-
+# Generate configs and scripts END
